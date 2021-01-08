b95694dcefb0bb56c3de735d104d06b22f3abe34
tom1k <tomascosta@ua.pt>
2021-01-08 19:48:40 +0000

    Testing tools, metrics for each word counter

--- code/counters.py
+++ code/counters.py
@@ -1,11 +1,14 @@
 import random
 import math
 import re
+from collections import defaultdict 
+import math
 class Counter:
     def __init__(self, file_path):
         # Initialize array and declare file_path
         self.file_path = file_path
         self.tokens = []
+        self.word_dict = defaultdict(lambda:  0)
         self.word_counter = 0
         self.counter_value = 0
 
@@ -14,9 +17,8 @@ class Counter:
         self.tokens = []
         self.word_counter = 0
         self.counter_value = 0
-
-    def getCount(self):
-        return self.word_counter
+        # we dont reset dict, but print out the values diving by number of trials, since the top10 words should be of all trials
+        #self.word_dict = defaultdict(lambda:  0)
 
     # Baseline function, all inherited classes will change this method, this is the method were we split into tokens
     def tokenize(self):
@@ -30,6 +32,10 @@ class Counter:
     def index(self):
         pass
 
+    def addToken(self,token):
+        self.word_dict[token] += 1
+
+
     def count(self):
         self.resetVars()
         self.tokenize()
@@ -37,25 +43,48 @@ class Counter:
 
     def getCounter(self):
         return self.counter_value
-
-    def getWordCounter(self):
+    
+    def getCount(self):
         return self.word_counter
 
-    def printCounter(self):
-        return f"Counter Value: {self.word_counter}"
+    def getTopWords(self, n_trials):
+        # returns 10 most frequent
+        ten_most_frequent = [(key,value / n_trials) for (key, value) in sorted(
+            self.word_dict.items(), key=lambda x: x[1], reverse=True)[:10]]
+        return ten_most_frequent
+
 class ExactCounter(Counter):
     def index(self):
         # treatment just for counter
+        for token in self.tokens:
+            self.addToken(token)
+            self.counter_value += 1
+        
+        # calculate word_value
         self.word_counter += len(self.tokens)
-        self.counter_value += len(self.tokens)
+    
+    def getDict(self):
+        return self.word_dict
 
 class HalfCounter(Counter):
     def index(self):
         # treatment just for counter
         # other way
-        random_array = [random.randint(0,1) for _ in range(len(self.tokens)+1)]
-        self.counter_value += sum(random_array)
-        self.word_counter += sum(random_array) * 2 # we multiply by 2, since we were predicting only half the events
+        random_array = [random.randint(0,1) for _ in range(len(self.tokens))]
+        for i in range(len(random_array)):
+            if random_array[i] == 1:
+                self.addToken(self.tokens[i])
+                self.counter_value += 1
+
+        # calculate word_value
+        self.word_counter += sum(random_array) * 2
+    
+    def getTopWords(self,n_trials):
+        # returns 10 most frequent
+        ten_most_frequent = [(key,value*2/n_trials) for (key, value) in sorted(
+            self.word_dict.items(), key=lambda x: x[1], reverse=True)[:10]]
+        return ten_most_frequent
+
 
 class LogCounter(Counter):
     def index(self):
@@ -66,7 +95,17 @@ class LogCounter(Counter):
             prob = 1/base**self.counter_value
             if random.random() < prob:
                 self.counter_value += 1
+                self.addToken(token)
 
         # this formula is provided in the slides as ( a^k–a + 1 ) / ( a –1 )
         k = self.counter_value
         self.word_counter = int((base**k - base + 1 ) / ( base-1 ))
+
+    def getTopWords(self, n_trials):
+        base = math.sqrt(2)
+
+        # returns 10 most frequent
+        ten_most_frequent = [(key, math.ceil((base**(value/n_trials) - base + 1) / (base - 1))) for (key, value) in sorted(
+            self.word_dict.items(), key=lambda x: x[1], reverse=True)[:10]]
+        return ten_most_frequent
+
--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ def usage():
 if __name__ == "__main__":
     # initialization of random variables
     type_counter = 'exact'
-    num_trials = None
+    num_trials = 1
     file_path = None
 
     try:
@@ -60,50 +60,56 @@ if __name__ == "__main__":
     elif type_counter == 'log':
         counter = LogCounter(file_path)
 
-    if num_trials != None:
-        # we calculate exact values to compare afterwards
-        counter_exact = ExactCounter(file_path)
-        counter_exact.count()
-        real_value = counter_exact.getCounter()
+    # we calculate exact values to compare afterwards
+    counter_exact = ExactCounter(file_path)
+    counter_exact.count()
+    real_value = counter_exact.getCounter()
+    exact_dict = counter_exact.getDict()
 
+    print(f"Executing {num_trials} trials on {type_counter} counter:\n")
+    tic = time.time()
+    trial_values = []
+    for i in range(num_trials):
+        counter.count()
+        val = counter.getCount()
+        trial_values.append(val)
 
-        print(f"Executing {num_trials} trials on {type_counter} counter:\n")
-        tic = time.time()
-        trial_values = []
-        for i in range(num_trials):
-            counter.count()
-            val = counter.getWordCounter()
-            trial_values.append(val)
+    # evaluate only the last words into a dict
+    top_words = counter.getTopWords(num_trials)
 
-        toc = time.time()
-        # We use NP to more efficiently calculate averages and std devs
-        trial_values = np.array(trial_values)
+    toc = time.time()
+    # We use NP to more efficiently calculate averages and std devs
+    trial_values = np.array(trial_values)
 
-        # Comparisons
-        relative_errors_array = np.absolute(trial_values-real_value) / real_value
-        max_re = np.max(relative_errors_array)
-        min_re = np.min(relative_errors_array)
-        mean_re = np.mean(relative_errors_array)
+    # Comparisons
+    relative_errors_array = np.absolute(trial_values-real_value) / real_value
+    max_re = np.max(relative_errors_array)
+    min_re = np.min(relative_errors_array)
+    mean_re = np.mean(relative_errors_array)
 
-        # Comparisons
-        accuracy_array = np.array(trial_values) / real_value
-        mean_acc = np.mean(accuracy_array)
+    # Comparisons
+    accuracy_array = np.array(trial_values) / real_value
+    mean_acc = np.mean(accuracy_array)
 
-        # Print out measures
-        print(f"\nMaximum relative error: {max_re:.3f}\nMinimum relative error: {min_re:.3f} \nMean relative error: {mean_re:.3f} \nMean accuracy ratio: {mean_acc:.3f}\n")
-        
-        unique, counts = np.unique(trial_values, return_counts=True)
-        count_dict = dict(zip(unique, counts))
+    # Print top words
+    print("Ten most frequent words: (average)")
+    print("Word \t\t Exact Count \t Est. Count \t Mean absolute error \t Mean relative error")
+    print("--------------------------------------------------------------------------------------------")
+    for word in top_words:
+        print(f"{word[0]} \t\t {exact_dict[word[0]]} \t\t {word[1]} \t\t {abs(word[1]-exact_dict[word[0]]):.1f} \t\t\t {abs(word[1]-exact_dict[word[0]])/ exact_dict[word[0]]*100:.2f}%")
 
-        # Print out table
-        for elem in unique:
-            count = count_dict[elem]
-            print(f"counter value: \t{elem},\t {count} times \t- {count/num_trials*100:.2f}%")
+    # Print out measures
+    print("\nGlobal counter measures:")
+    print(f"Maximum relative error: {max_re*100:.3f}%\nMinimum relative error: {min_re* 100:.3f}% \nMean relative error: {mean_re*100:.3f}% \nMean accuracy ratio: {mean_acc*100:.3f}%\n")
+    
+    unique, counts = np.unique(trial_values, return_counts=True)
+    count_dict = dict(zip(unique, counts))
 
-        print(f"Calculated in {toc-tic:.2f} seconds")
-        # we dont go any further, this is the testing batch
-        sys.exit()
+    # Print out table
+    for elem in unique:
+        count = count_dict[elem]
+        #print(f"counter value: \t{elem},\t {count} times \t- {count/num_trials*100:.2f}%")
 
-    # Counting needs to happen before
-    counter.count()
-    print(counter.printCounter())
+    print(f"Calculated in {toc-tic:.2f} seconds")
+    # we dont go any further, this is the testing batch
+    sys.exit()
